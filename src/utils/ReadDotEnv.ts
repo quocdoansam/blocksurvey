aff16426bd4d38fa1cd0ddab8c6cfb371b4d53ef
Sam <quocdoansam.me@gmail.com>
2025-05-22 01:49:01 +0700

    fix: correct error message in readDotEnv function to use the correct variable name

--- src/utils/ReadDotEnv.ts
+++ src/utils/ReadDotEnv.ts
@@ -2,6 +2,6 @@ import type { EnvKey } from "@/types/EnvKey";
 
 export const readDotEnv = (key: EnvKey) => {
   const value = import.meta.env[key];
-  if (!value) throw new Error(`Missing environment variable: ${name}`);
+  if (!value) throw new Error(`Missing environment variable: ${key}`);
   return value;
 };
