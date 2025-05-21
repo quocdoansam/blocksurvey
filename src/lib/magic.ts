09d59c82798d8ded30effc3a302dd8c02a3d32d4
Sam <quocdoansam.me@gmail.com>
2025-05-22 04:30:24 +0700

    feat: integrate @magic-ext/oauth2 for OAuth authentication

--- src/lib/magic.ts
+++ src/lib/magic.ts
@@ -1,4 +1,5 @@
 import { readDotEnv } from "@/utils/ReadDotEnv";
+import { OAuthExtension } from "@magic-ext/oauth2";
 import { Magic } from "magic-sdk";
 
 const PUBLIC_KEY = readDotEnv("VITE_MAGIC_PUBLIC_KEY");
@@ -6,6 +7,7 @@ const RPC_URL = readDotEnv("VITE_RPC_URL");
 const CHAIN_ID = readDotEnv("VITE_CHAIN_ID");
 
 export const magic = new Magic(PUBLIC_KEY, {
+  extensions: [new OAuthExtension()],
   network: {
     rpcUrl: RPC_URL,
     chainId: CHAIN_ID,
